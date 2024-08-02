75992c8cb7319faf6ec511a45a30ebeffb8973ea
Serdyukova <lavrovasd@mail.ru>
2024-08-02 22:29:05 +0400

    initial

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+      int moneySpent = 13676;
+      int oneMileMoney = 20;
+      
+      int bonuses = moneySpent / oneMileMoney;
+      
+      System.out.println(bonuses);
+  }
+}
